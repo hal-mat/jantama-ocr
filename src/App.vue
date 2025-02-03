ac070be86799b3934c57000fa21f351e8a9492e6
hal-mat <80268308+hal-mat@users.noreply.github.com>
2025-02-03 11:56:13 +0900

    jimp imput check

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,44 @@
 <script setup lang="ts">
   import { Jimp } from "jimp";
+  import cv from "@techstark/opencv-js";
 
   // https://jimp-dev.github.io/jimp/guides/browser/
-  // async function inputCheck(file: File, outputCanvasId: string): Promise<void> {
-  // console.log("called inputCheck");
-  // inputからファイルを読み込む
-  // const arrayBuffer = await file.arrayBuffer();
-  // Jimpで読み込み
-  // const image = await Jimp.read(file);
-  // canvasに描画
-  // const imageData = new ImageData(Uint8ClampedArray.from(image.bitmap.data), image.bitmap.width, image.bitmap.height);
-  // const outputCanvas: HTMLCanvasElement = document.getElementById(outputCanvasId) as HTMLCanvasElement;
-  // const ctx: CanvasRenderingContext2D = outputCanvas.getContext("2d") as CanvasRenderingContext2D;
-  // ctx.putImageData(imageData, 0, 0);
-  // }
+  async function inputCheck(file: File, outputCanvasId: string): Promise<void> {
+    console.log("called inputCheck");
+    // inputからファイルを読み込む
+    const fileArrayBuffer: ArrayBuffer = await file.arrayBuffer();
+    // Jimpで読み込み
+    const readImage = await Jimp.read(fileArrayBuffer);
+    // canvasに描画
+    const inputImageData: ImageData = new ImageData(
+      Uint8ClampedArray.from(readImage.bitmap.data),
+      readImage.bitmap.width,
+      readImage.bitmap.height
+    );
+    const outputCanvas: HTMLCanvasElement = document.getElementById(outputCanvasId) as HTMLCanvasElement;
+    const ctx: CanvasRenderingContext2D = outputCanvas.getContext("2d") as CanvasRenderingContext2D;
+    ctx.putImageData(inputImageData, 0, 0);
+  }
   // https://tekrog.com/jimp-with-typescript-on-browser
-  // function showGray(file: File, outputCanvasId: string): void {
-  // const outputCanvas = document.getElementById(outputCanvasId) as HTMLCanvasElement;
-  // const ctx = outputCanvas.getContext("2d");
-  // const arrayBuffer = file.arrayBuffer();
-  // const image = Jimp.read(Buffer.from(arrayBuffer));
-  // image.resize(256, 256);
-  // const imageData = new ImageData(Uint8ClampedArray.from(image.bitmap.data), image.bitmap.width, image.bitmap.height);
-  // ctx.putImageData(imageData, 0, 0);
-  // }
 
   // https://qiita.com/xenepic/items/4aa94bece50364002497
   let uploadedImage: File | null = null;
 
-  async function handleFileUpload(e: Event): void {
-    const target = e.target as HTMLInputElement;
+  async function handleFileUpload(e: Event): Promise<void> {
+    console.log("called handleFileUpload");
+    // ここでnullの危険性を消す
+    const e_target = e.target as EventTarget & { files: FileList };
+    const e_target_files: FileList = e_target.files;
 
-    if (!target.files || target.files.length == 0) {
+    if (!e_target_files || e_target_files.length == 0) {
       return;
     }
-    uploadedImage = target.files[0]; // -1不可
-    // https://qiita.com/mmorino/items/6e6ed34bfb874c80a7f2
-    let reader: FileReader = new FileReader();
-    reader.onload = (target) => {
-      createImage(target.result);
-    };
 
+    uploadedImage = e_target_files[0] as File; // -1不可
     console.log(typeof uploadedImage);
-    // reader.readAsDataURL(uploadedImage);
+    // https://qiita.com/mmorino/items/6e6ed34bfb874c80a7f2
     // https://jimp-dev.github.io/jimp/guides/browser/
-  }
-  function createImage(file: File) {
-    console.log("createImage");
-    // 画像サイズ調整
-    Jimp.read(file, function (err, image) {
-      image.getBase64(Jimp.MIME_PNG, function (err, src) {
-        uploadedImageBlob = base64ToBlob(src);
-        previewUploadedSrc = src;
-      });
-    });
-  }
-
-  function base64ToBlob(base64) {
-    console.log("base64ToBlob");
-    const bin = atob(base64.replace(/^.*,/, ""));
-    const buffer = new Uint8Array(bin.length);
-    for (let i = 0; i < bin.length; i++) {
-      buffer[i] = bin.charCodeAt(i);
-    }
-    return new Blob([buffer.buffer], {
-      type: "image/png",
-    });
+    inputCheck(uploadedImage, "input-check");
   }
 </script>
 
